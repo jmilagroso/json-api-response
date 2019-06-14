2630a3865302a33c968eb72e4e3cd1e83f239976
jmilagroso <j.milagroso@gmail.com>
2019-06-14 11:36:10 +0800

    feature/json-type-override-support Added support on json api type override.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 const Typcheckjs = require('typcheckjs');
 
 module.exports = {
-  serialize(payload, serializeName = 'serialize') {
+  serialize(payload, t) {
     let keys = [];
     if (Typcheckjs.isArray(payload)) {
       keys = Object.keys(payload[0]);
@@ -10,7 +10,9 @@ module.exports = {
       keys = Object.keys(payload);
     }
 
-    const json = new JSONAPISerializer(serializeName, {
+    const type = (t === '' || Typcheckjs.isNull(t) || Typcheckjs.isUndefined(t)) ? 'response' : t;
+
+    const json = new JSONAPISerializer(type, {
       attributes: keys,
     }).serialize(payload);
 
@@ -18,6 +20,7 @@ module.exports = {
   },
   handler(
     data = {},
+    type,
     status = 200,
     contentType = 'application/vnd.api+json',
     responseHeaders = {
@@ -31,131 +34,144 @@ module.exports = {
         'Content-Type': contentType,
         responseHeaders,
       },
-      body: JSON.stringify(this.serialize(data)),
+      body: JSON.stringify(this.serialize(data, type)),
     };
   },
   ok(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 200, contentType, responseHeaders);
+    return this.handler(data, type, 200, contentType, responseHeaders);
   },
   created(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 201, contentType, responseHeaders);
+    return this.handler(data, type, 201, contentType, responseHeaders);
   },
   accepted(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 201, contentType, responseHeaders);
+    return this.handler(data, type, 201, contentType, responseHeaders);
   },
   partialContent(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 206, contentType, responseHeaders);
+    return this.handler(data, type, 206, contentType, responseHeaders);
   },
   movedPermanently(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 301, contentType, responseHeaders);
+    return this.handler(data, type, 301, contentType, responseHeaders);
   },
   notModified(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 304, contentType, responseHeaders);
+    return this.handler(data, type, 304, contentType, responseHeaders);
   },
   badRequest(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 400, contentType, responseHeaders);
+    return this.handler(data, type, 400, contentType, responseHeaders);
   },
   unauthorized(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 401, contentType, responseHeaders);
+    return this.handler(data, type, 401, contentType, responseHeaders);
   },
   forbidden(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 403, contentType, responseHeaders);
+    return this.handler(data, type, 403, contentType, responseHeaders);
   },
   notFound(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 404, contentType, responseHeaders);
+    return this.handler(data, type, 404, contentType, responseHeaders);
   },
   methodNotAllowed(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 405, contentType, responseHeaders);
+    return this.handler(data, type, 405, contentType, responseHeaders);
   },
   internalServerError(
     data = {},
+    type = '',
     contentType = 'application/vnd.api+json',
     responseHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, 500, contentType, responseHeaders);
+    return this.handler(data, type, 500, contentType, responseHeaders);
   },
   custom(
     data = {},
+    type = '',
     status = 200,
     contentType = 'application/vnd.api+json',
     responseHeaders = {
@@ -163,6 +179,6 @@ module.exports = {
       'Access-Control-Allow-Credentials': true,
     },
   ) {
-    return this.handler(data, status, contentType, responseHeaders);
+    return this.handler(data, type, status, contentType, responseHeaders);
   },
 };
